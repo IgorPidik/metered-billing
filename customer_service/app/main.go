ea457b0ac7e14d9997c6cf732e8d3e354449164a
Igor Pidik <igorPidik@gmail.com>
2022-09-11 12:01:12 +0200

    customer service running

--- customer_service/app/main.go
+++ customer_service/app/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"net"
 
@@ -11,13 +9,9 @@ import (
 	"google.golang.org/grpc"
 )
 
-var (
-	port = flag.Int("port", 50051, "The server port")
-)
-
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	log.Println("Starting customer service...")
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
